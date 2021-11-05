d469d42ed07e425e6f634d90e536001af0701451
Aleksandr Orlov <aleksandr.orlov.dev@gmail.com>
2021-11-05 10:05:24 +0300

    Delete keys directory

--- keys/keys.prod.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI,
-  SECTION_SECRET: process.env.SECTION_SECRET,
-  SENDGRID_API_KEY:
-    process.env.SENDGRID_API_KEY,
-  EMAIL_FROM: process.env.EMAIL_FROM,
-  BASE_URL: process.env.BASE_URL,
-};
