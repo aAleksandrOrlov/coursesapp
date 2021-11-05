c4f922f3b010572b93c5bc693cf02c56e4e4d629
Aleksandr Lin <jolym1nk@gmail.com>
2021-11-05 10:08:34 +0300

    add keys

---
+++ keys/keys.prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  SECTION_SECRET: process.env.SECTION_SECRET,
+  SENDGRID_API_KEY:
+    process.env.SENDGRID_API_KEY,
+  EMAIL_FROM: process.env.EMAIL_FROM,
+  BASE_URL: process.env.BASE_URL,
+};
